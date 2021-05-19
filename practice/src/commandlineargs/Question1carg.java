47e18db252948e4f7e31e9025ccbf7dc7bd49203
Raghavendra <Raghavendra@DESKTOP-EV6BELO>
2021-05-19 16:53:13 +0530

    practice of command line args

---
+++ practice/src/commandlineargs/Question1carg.java
@@ -0,0 +1,9 @@
+package commandlineargs;
+
+public class Question1carg {
+	public static void main(String[] args)
+	{
+		System.out.println(args[0]+" Technologies "+args[1]);
+	}
+
+}
---
+++ practice/src/commandlineargs/Question3carg.java
@@ -0,0 +1,12 @@
+package commandlineargs;
+
+public class Question3carg {
+public static void main(String[] args)
+{
+	int a,b;
+	a=Integer.parseInt(args[0]);
+	b=Integer.parseInt(args[1]);
+	System.out.println(+(a+b));
+	
+}
+}
